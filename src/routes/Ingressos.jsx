65119f14e25d6b29138991c6ce689260070763f7
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-17 15:51:20 -0300

    criando a pagina de ingressos

--- src/routes/Ingressos.jsx
+++ src/routes/Ingressos.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
 
 const Ingressos = () => {
   return (
-    <div>Ingressos</div>
+    <section className="ingressos-container">
+      <div className="banner-ingressos">
+        <h1>Ingressos</h1>
+        <p>Garanta agora seus ingressos para assistir a Fórmula E ao vivo.</p>
+        <p>Nos vemos na pista!</p>
+      </div>
+      <div className="datas">
+        <div className="cards-datas">
+          <p>Cidade</p>
+          <p>data</p>
+          <p>etapa</p>
+          <img src="" alt="local" />
+          <Link>Comprar Ingressos</Link>
+        </div>
+        <div className="cards-datas">
+          <p>Cidade</p>
+          <p>data</p>
+          <p>etapa</p>
+          <img src="" alt="local" />
+          <Link>Comprar Ingressos</Link>
+        </div>
+        <div className="cards-datas">
+          <p>Cidade</p>
+          <p>data</p>
+          <p>etapa</p>
+          <img src="" alt="local" />
+          <Link>Comprar Ingressos</Link>
+        </div>
+        <div className="cards-datas">
+          <p>Cidade</p>
+          <p>data</p>
+          <p>etapa</p>
+          <img src="" alt="local" />
+          <Link>Comprar Ingressos</Link>
+        </div>
+        <div className="cards-datas">
+          <p>Cidade</p>
+          <p>data</p>
+          <p>etapa</p>
+          <img src="" alt="local" />
+          <Link>Comprar Ingressos</Link>
+        </div>
+        <div className="cards-datas">
+          <p>Cidade</p>
+          <p>data</p>
+          <p>etapa</p>
+          <img src="" alt="local" />
+          <Link>Comprar Ingressos</Link>
+        </div>
+      </div>
+      <div className="vantagens">
+        <h2>Assistir a uma corrida de carros elétricos, como a Fórmula E, é uma experiência única e emocionante: </h2>
+        <p>
+          Cenários Urbanos: Corridas ocorrem no coração das cidades.
+          Sustentabilidade: Eventos ecológicos com uso de energia renovável.
+          Silêncio Relativo: Zumbido dos motores elétricos em vez do rugido de motores a combustão.
+          Interatividade: Tecnologias como o FanBoost envolvem os fãs.
+          Corridas Intensas: Ação concentrada em 45 minutos a uma hora.
+          Pilotos de Elite: Inclusão de ex-pilotos de Fórmula 1.
+          Design Futurista: Carros com estética moderna.
+          Iluminação e Efeitos: Corridas noturnas com efeitos visuais impressionantes.
+          Diversidade e Inclusão: Ambiente acolhedor e diversificado.
+          Engajamento Global: Corridas em várias cidades ao redor do mundo
+        </p>
+        <h2>Junte-se a nós para vivenciar a emoção das corridas de carros elétricos!</h2>
+      </div>
+
+    </section>
   )
 }
 
