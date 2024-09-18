a5db44e52bdecf7ea1207b240bf56da08d8d086c
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-18 10:14:58 -0300

    criando Json de dados e modificando as paginas desafios, login, sobre

--- src/routes/Desafios.jsx
+++ src/routes/Desafios.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+
 
 const Desafios = () => {
   return (
-    <div>Desafios</div>
+    <section className="container-desafios">
+      <h1>Desafios e Soluções</h1>
+      <div className="texto-1">
+        <p>Uma das maiores diferenças entre a Fórmula E e outras competições automobilísticas é o som dos carros. Enquanto os motores a combustão são conhecidos por seus rugidos altos e emocionantes, os motores elétricos da Fórmula E produzem muito menos barulho. Isso pode ser uma desvantagem para os fãs acostumados à experiência sonora das corridas tradicionais, já que o som é uma parte importante da emoção e do espetáculo.</p>
+        <p>Para criar uma experiência mais emocionante para os fãs, a Fórmula E terá que explorar  o uso de sons artificiais. Esses sons são projetados para simular a intensidade e a emoção dos motores a combustão, mas de uma maneira que seja mais ambientalmente amigável. Além disso, o menor ruído permite que os espectadores apreciem outros sons da corrida, como o contato dos pneus com a pista e a interação entre os pilotos e suas equipes, oferecendo uma nova dimensão à experiência da corrida.</p>
+        <img src="" alt="ondas" />
+      </div>
+      <div className="texto-2">
+        <img src="" alt="redes" />
+        <p>Outro problema é o desconhecimento sobre a História da Fórmula E.
+        Embora a Fórmula E tenha feito avanços significativos desde seu lançamento em 2014, ainda há um grande número de pessoas que não conhecem sua história e seus objetivos. A falta de familiaridade com a evolução da Fórmula E e sua missão de promover a sustentabilidade e a inovação tecnológica pode limitar o engajamento e o interesse do público.</p>
+        <p>
+        A Fórmula E tem investido fortemente em campanhas de marketing e iniciativas educacionais para aumentar a conscientização sobre sua história e objetivos. Isso inclui a criação de documentários, séries de vídeos e artigos informativos que detalham a origem da Fórmula E, seus marcos importantes e suas contribuições para a sustentabilidade. Além disso, a Fórmula E tem fortalecido sua presença nas redes sociais e colaborado com influenciadores e mídias especializadas para alcançar um público mais amplo e diversificado.
+        </p>
+      </div>
+      <Link to='/sobre'>Conhecer nossa história</Link>
+      
+      
+
+
+    </section>
   )
 }
 
--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,8 +1,91 @@
-import React from 'react'
+import {useRef, useState, useEffect} from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 const Login = () => {
+  const email = useRef();
+  const senha = useRef();
+
+  const [usuarios, setUsuarios] = useState([]);
+  const navigate = useNavigate();
+
+  function validar(){
+    for(let i=0;i<usuarios.length;i++){
+      if(
+        usuarios[i].usuario == usuario.current.value &&
+        usuarios[i].senha == senha.current.value
+      ){
+        return true;
+      }
+    }
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+
+    if(validar()){
+      let token =
+        Math.random().toString(16).substring(2)+
+        Math.random().toString.substring(2)
+        sessionStorage.setItem("usuario", usuario.current.value);
+        sessionStorage.setItem("senha", token);
+        //CHAMAR PÁGINA LOGADA
+        navigate('/')
+    }else{
+      alert("usuario/senha inválidos")
+    }
+
+    useEffect(()=>{
+      fetch("http://localhost")
+
+    })
+
+
+
+
+  }
   return (
-    <div>Login</div>
+    <section className='container-login'>
+    <form className='login-form' onSubmit={handleSubmit}>
+      <div className='form-group'>
+        <label htmlFor="email">E-mail</label>
+        <input 
+
+          type="text"
+          id="email"
+          ref = {email}
+          placeholder='Digite seu E-mail'
+
+         />
+      </div>
+      <div className='form-group'>
+        <label htmlFor="senha">Senha</label>
+        <input 
+
+          type="password"
+          id="senha"
+          ref = {senha}
+          placeholder='Digite sua Senha'
+
+         />
+      </div>
+
+      <div className='login-btn'>
+        <button type='submit' className='login-form-btn'>Login</button>
+      </div>
+
+      <ul className='utilidades'>
+        <li>
+          <span className='text1'>Esqueceu sua senha?</span>
+        </li>
+        <li>
+          <span className='text1'>Não tem conta?</span>
+          <Link to='/cadastro' className='text2'>Criar</Link>
+        </li>
+
+      </ul>
+    </form>
+
+    </section>
   )
 }
 
