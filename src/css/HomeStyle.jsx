51f122527d1c6431ba04da8a205cee0c2fc19b79
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-20 00:07:54 -0300

    Estilização da pagina Home

--- src/css/HomeStyle.jsx
+++ src/css/HomeStyle.jsx
@@ -5,6 +5,229 @@ export const HomeStyle = styled.section`
 .container-home{
     width:100%;
     min-height:100vh;
+    background:var(--bg-color);
+
 }
+.banner-home{
+    width: 100%;
+    min-height: 40vh;
+    display: grid;
+    grid-template-areas: 
+        "logo carro"
+        "texto carro"
+        " btn     . "
+    ;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr 0.5fr;
+    column-gap: 80px;
+    background: var(--azul-claro);
+   
+}
+.logo{
+    grid-area: logo;
+    max-width: 100% ;
+    height:auto;  
+    align-self: center;
+    justify-self: center;
+}
+.carro1{
+    grid-area: carro;
+    max-width: 100%;
+    height: auto;
+    display: flex;
+    align-self: center;
+    justify-self: center;
+}
+.text{
+    grid-area: texto;
+    color:var(--bg-color);
+    height: auto;
+    text-align: justify;
+    padding-left: 50px;
+    justify-self: center;
+    align-self:center;
+    font-size:1.2rem;
+}
+.btn-sabermais{
+    grid-area: btn;
+    justify-self: center;
+    align-self: center;
+    background: var(--bg-color);
+    text-decoration:underline;
+    text-decoration-thickness: 2px;
+    color: var(--azul-claro);width:266px;
+    height: 45px;
+    border-radius: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.1rem;
+}
+.section-title{
+    width:80%;
+    justify-self: flex-end;
+    background:var(--bg-color);
+    position: relative;
+    clip-path: polygon(10% 0, 100% 0, 100% 100%, 0% 100%);
+    display:flex;
+    justify-content: center;
+    align-items: center;
+}
+.section-title h1{
+    font-size: 2.5rem;
+    color: var(--azul-escuro);
+
+}
+
+.calendario-corridas{
+    width:100%;
+    min-height:30vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap:30px;
+    flex-wrap: wrap;
+    padding :40px 150px;
+
+
+}
+.card-corridas{
+    background:#fff;
+    width: 200px;
+    height:300px;
+    box-shadow: 0 4px 8px black;
+    border-radius: 40px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 20px;
+    color:var(--azul-escuro);
+    font-weight: 800;
+    gap:10px;
+}
+.card-corridas img{
+    width:100%;
+    height:auto;
+    border-radius: 8px;
+    
+}
+.btn{
+    width: 100%;
+    height: 50px;
+    background: var(--azul-claro);
+    border-radius:15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color:var(--bg-color);
+    font-weight: 600;
+}
+
+.info-sust{
+    width:100%;
+    min-height:50vh;
+    background-image: url(src/assets/foto-fundo.png);
+    background-size: cover;
+    background-position:center center;
+    background-repeat: no-repeat;
+    display:flex;
+    flex-direction: column;
+    position:relative;
+    justify-content:flex-start;
+    align-items:flex-start;
+    gap:90px;
+    
+    
+}
+.titulo-sust{
+    color: var(--azul-escuro);
+    padding: 20px 20px;
+    
+}
+.titulo-sust h2{
+    font-size:2.2rem;
+    font-weight:800;
+}
+.titulo-sust h3{
+    font-size:1.8rem;
+    font-weight: 400;
+}
+
+.texto-sust{
+    width: 80%;
+    height:50%;
+    align-self: center;
+    justify-self: center;
+    text-align:justify;
+    background: var(--azul-claro);
+    color:var(--bg-color);
+    border-radius:60px ;
+    
+}
+.texto-sust p{
+    padding:25px;
+}
+.section-title2{
+
+    background:var(--bg-color);
+    position: absolute;
+    bottom:0;
+    left:0;
+    clip-path: polygon(0 0, 90% 0, 100% 100%, 0% 100%);
+    width: 30%;
+    height:60px;
+    display: flex;
+    padding-left:60px;
+    align-items: center;
+    color: var(--azul-escuro);
+    font-size:1.8rem;
+    
+
+}
+.parceiros{
+    width:100%;
+    min-height:40vh;
+
+}
+.parceiros-1{
+    display: flex;
+    height: 10vh;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 2px solid var(--azul-escuro);
+    
+}
+.parceiros-2{
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+    border-bottom: 2px solid var(--azul-escuro);
+    height: 10vh;
+    
+}
+
+.parceiros-2 img{
+    width: 100%;
+    max-width:100px;
+    height: auto;
+
+}
+.parceiros-3{
+    display:flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 10vh;
+}
+.parceiros-3 img{
+    width: 100%;
+    max-width:100px;
+    height: auto;
+}
+
+
+
+
+
 
 `
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom"
 import { HomeStyle } from "../css/HomeStyle"
+import logo from "../assets/formula-e-logo-2.png"
+import carro from '../assets/carro-pag-1.png'
+import londres from '../assets/londres.png'
+import usa from '../assets/portland.png'
+
 
 
 const Home = () => {
@@ -7,81 +12,86 @@ const Home = () => {
     <HomeStyle>
         <section className="container-home">
             <div className="banner-home"> 
-            <img src="" alt="logo-formulaE" />
-            <img src="" alt="carro" />
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum veniam ex fuga fugiat! Natus voluptates saepe, vero, itaque dolore corporis mollitia beatae iste ducimus vel quas maiores illo quos sit adipisci doloribus laudantium culpa nobis. Culpa molestiae explicabo quae, at tempora, consectetur accusamus natus perspiciatis eligendi accusantium perferendis quos soluta.</p>
-            <Link to='/sobre' className="btn-sabermais">Quero saber mais</Link>
-
-            <h1 className="section-title">
-                Futuras Corridas
-            </h1>
+                <img src={logo} alt="logo-formulaE" className="logo"/>
+                <img src={carro} alt="carro" className="carro1"/>
+                <p className="text">A Fórmula E é a principal categoria de automobilismo de carros elétricos, lançada em 2014. Promovendo a mobilidade sustentável e a inovação tecnológica, ela realiza corridas emocionantes em circuitos urbanos globais e serve como um laboratório para desenvolver tecnologias avançadas para veículos elétricos de produção em massa.</p>
+                <Link to='/sobre' className="btn-sabermais">Quero saber mais!</Link>
+                <div className="section-title">
+                    <h1>
+                    Futuras Corridas
+                </h1>
+                </div>
+            
             </div>
             <div className="calendario-corridas">
                 <div className="card-corridas">
-                    <p>cidade da corrida</p>
-                    <p>data</p>
-                    <p>etapa</p>
-                    <img src="" alt="local da corrida" />
-                    <Link to='/ingressos'></Link>
+                    <p>Portland</p>
+                    <p>21/09</p>
+                    <p>ROUND 13</p>
+                    <img src={usa} alt="local da corrida" />
+                    <Link to='/ingressos' className='btn'>Comprar Ingressos</Link>
                 </div>
                 <div className="card-corridas">
-                    <p>cidade da corrida</p>
-                    <p>data</p>
-                    <p>etapa</p>
-                    <img src="" alt="local da corrida" />
-                    <Link to='/ingressos'></Link>
+                    <p>Portland</p>
+                    <p>05/10</p>
+                    <p>ROUND 14</p>
+                    <img src={usa} alt="local da corrida" />
+                    <Link to='/ingressos' className='btn'>Comprar Ingressos</Link>
                 </div>
                 <div className="card-corridas">
-                    <p>cidade da corrida</p>
-                    <p>data</p>
-                    <p>etapa</p>
-                    <img src="" alt="local da corrida" />
-                    <Link to='/ingressos'></Link>
+                    <p>Londres</p>
+                    <p>14/10</p>
+                    <p>ROUND 15</p>
+                    <img src={londres} alt="local da corrida" />
+                    <Link to='/ingressos' className='btn'>Comprar Ingressos</Link>
                 </div>
                 <div className="card-corridas">
-                    <p>cidade da corrida</p>
-                    <p>data</p>
-                    <p>etapa</p>
-                    <img src="" alt="local da corrida" />
-                    <Link to='/ingressos'></Link>
+                    <p>Londres</p>
+                    <p>01/11</p>
+                    <p>ROUND 16</p>
+                    <img src={londres} alt="local da corrida" />
+                    <Link to='/ingressos' className='btn'>Comprar Ingressos</Link>
                 </div>
 
             </div>
 
             <div className="info-sust">
-                <h2>Sustentabilidade na Fórmula E:</h2>
-                <h3>Rumo a um Futuro Limpo e Sustentável</h3>
-                <div className="texto-sust">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur nemo molestias quidem, vero illum earum in suscipit necessitatibus tempore aspernatur rem est harum enim distinctio deserunt, recusandae pariatur quae minus, facere perferendis perspiciatis aut quod! Perferendis asperiores deserunt distinctio sint molestiae sequi? Veniam odio quasi sunt autem delectus. Nihil, dolore.</p>
+                <div className='titulo-sust'>
+                    <h2>Sustentabilidade na Fórmula E:</h2>
+                    <h3>Rumo a um Futuro Limpo e Sustentável</h3>
                 </div>
-                <h1 className="section-title">Parceiros</h1>
+                
+                <div className="texto-sust">
+                    <p>A Fórmula E lidera a sustentabilidade no automobilismo com veículos elétricos, reduzindo emissões e usando circuitos urbanos. Funciona como um laboratório para tecnologias limpas, que são aplicadas em veículos de rua. Promove a conscientização ambiental e colabora com organizações e empresas para compartilhar recursos e práticas sustentáveis.  A Fórmula E está moldando um futuro de mobilidade limpa e inspirando ações sustentáveis em todo o mundo.</p>
+                </div>
+                <h1 className="section-title2">Parceiros</h1>
             </div>
             <div className="parceiros">
                 <div className="parceiros-1">
-                    <img src="" alt="ABB" />
+                    <img src="src/assets/parceiros/ABB-logo.png" alt="ABB" />
                 </div>
                 <div className="parceiros-2">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <img src="src\assets\parceiros\Julius-baer-logo.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\HK_2019_Logo-1.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\pif_colour.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\1200px-SABIC_Logo_RGB.png" alt="parceiros" />
                 </div>
-                <div className="parceiros-3">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                <div className="parceiros-2">
+                    <img src="src\assets\parceiros\Allianz-sponsor.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\antofagasta-minerals-logo.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\Bosch-Logo-1925.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\DHL_Logo.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\google-cloud.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\Ifosys-Logo.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\SaaudiaLogo.png" alt="parceiros" />
 
                 </div>
-                <div className="parceiros-4">
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                <div className="parceiros-3">
+                    <img src="src\assets\parceiros\Aggreko-Logo-RGB-01.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\Hackett_Sport_Logo_Black.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\southwire-logo.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\taittinger_logo.png" alt="parceiros" />
+                    <img src="src\assets\parceiros\Tata-Communications-Logo-Stacked-Blue-New.png" alt="parceiros" />
                 </div>
 
             </div>
