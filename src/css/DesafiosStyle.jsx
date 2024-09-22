084341673ba081c7dd87ed2dae9b8953ee8a27fa
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-22 20:38:26 -0300

    estilizando a pagina de desafios

---
+++ src/css/DesafiosStyle.jsx
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+
+export const DesafiosStyle = styled.section `
+
+    
+
+    .container-desafios{
+        width:100%;
+        min-height:100vh;
+        
+
+    }
+    .bg{
+        background: var(--azul-claro);
+        color:var(--azul-claro);
+        height:4vh;
+        width:100%;
+    }
+    
+    .titulo-container{
+        width:100%;
+        height:5vh;
+        background:var(--bg-color);
+        display: flex;
+        align-items: center;
+        justify-content:center;
+        
+    }
+    .titulo{
+        background: var(--azul-escuro);
+        color:var(--bg-color);
+        width:30%;
+        height:150%;
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        border-radius:50px;
+        font-size: 2rem;
+        
+    }
+    .section-1{
+        width:100%;
+        min-height:50vh;
+        display: grid;
+        grid-template-areas: "texto img";
+        grid-template-columns: 1.5fr 1fr;
+        grid-template-rows: 1fr;
+        background:var(--azul-claro);
+        
+    }
+    .texto-1{
+        
+        grid-area:texto;
+        display: flex;
+        flex-direction: column;
+        font-size: 1rem;
+        justify-content: space-evenly;
+        text-align: justify;
+        color:var(--bg-color);
+        padding:100px;
+        gap:30px;
+        
+    }
+    .section-1 img{
+        width:100%;
+        height: auto;
+        padding: 80px;
+        align-self: center;
+    }
+
+    .section-2{
+        width:100%;
+        min-height:50vh;
+        display: grid;
+        grid-template-areas: "img texto"
+            "btn btn"
+        ;
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: 1fr 0.5fr;
+        background:var(--azul-claro);
+    }
+
+    .section-2 img{
+        grid-area:img;
+        width: 100%;
+        height: auto;
+
+    }
+    .texto-2{
+        text-align: justify;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding:50px;
+        gap:20px;
+        color:var(--bg-color);
+    }
+    .btn{
+        grid-area:btn;
+        align-self: center;
+        justify-self:center;
+        
+        background: var(--azul-escuro);
+        color:var(--bg-color);
+        width: 250px ;
+        height: 50px;
+        border-radius: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-decoration: underline;
+        text-decoration-thickness:2px;
+        font-size:1.1rem;
+        font-weight: 600;
+    }
+
+`
--- src/routes/Desafios.jsx
+++ src/routes/Desafios.jsx
@@ -1,29 +1,50 @@
 import { Link } from "react-router-dom"
+import { DesafiosStyle } from "../css/DesafiosStyle"
 
 
 const Desafios = () => {
   return (
-    <section className="container-desafios">
-      <h1>Desafios e Soluções</h1>
-      <div className="texto-1">
-        <p>Uma das maiores diferenças entre a Fórmula E e outras competições automobilísticas é o som dos carros. Enquanto os motores a combustão são conhecidos por seus rugidos altos e emocionantes, os motores elétricos da Fórmula E produzem muito menos barulho. Isso pode ser uma desvantagem para os fãs acostumados à experiência sonora das corridas tradicionais, já que o som é uma parte importante da emoção e do espetáculo.</p>
-        <p>Para criar uma experiência mais emocionante para os fãs, a Fórmula E terá que explorar  o uso de sons artificiais. Esses sons são projetados para simular a intensidade e a emoção dos motores a combustão, mas de uma maneira que seja mais ambientalmente amigável. Além disso, o menor ruído permite que os espectadores apreciem outros sons da corrida, como o contato dos pneus com a pista e a interação entre os pilotos e suas equipes, oferecendo uma nova dimensão à experiência da corrida.</p>
-        <img src="" alt="ondas" />
-      </div>
-      <div className="texto-2">
-        <img src="" alt="redes" />
-        <p>Outro problema é o desconhecimento sobre a História da Fórmula E.
-        Embora a Fórmula E tenha feito avanços significativos desde seu lançamento em 2014, ainda há um grande número de pessoas que não conhecem sua história e seus objetivos. A falta de familiaridade com a evolução da Fórmula E e sua missão de promover a sustentabilidade e a inovação tecnológica pode limitar o engajamento e o interesse do público.</p>
-        <p>
-        A Fórmula E tem investido fortemente em campanhas de marketing e iniciativas educacionais para aumentar a conscientização sobre sua história e objetivos. Isso inclui a criação de documentários, séries de vídeos e artigos informativos que detalham a origem da Fórmula E, seus marcos importantes e suas contribuições para a sustentabilidade. Além disso, a Fórmula E tem fortalecido sua presença nas redes sociais e colaborado com influenciadores e mídias especializadas para alcançar um público mais amplo e diversificado.
-        </p>
-      </div>
-      <Link to='/sobre'>Conhecer nossa história</Link>
+    <DesafiosStyle>
+      <section className="container-desafios">
+        <div className="bg">
+            <p>l</p>
+        </div>
+
+        
+        <div className="titulo-container">
+        <h1 className="titulo">Desafios e Soluções</h1>
+        </div>
+        
+        
+        <section className="section-1">
+          <div className="texto-1">
+            <p>Uma das maiores diferenças entre a Fórmula E e outras competições automobilísticas é o som dos carros. Enquanto os motores a combustão são conhecidos por seus rugidos altos e emocionantes, os motores elétricos da Fórmula E produzem muito menos barulho. Isso pode ser uma desvantagem para os fãs acostumados à experiência sonora das corridas tradicionais, já que o som é uma parte importante da emoção e do espetáculo.</p>
+            <p>Para criar uma experiência mais emocionante para os fãs, a Fórmula E terá que explorar  o uso de sons artificiais. Esses sons são projetados para simular a intensidade e a emoção dos motores a combustão, mas de uma maneira que seja mais ambientalmente amigável. Além disso, o menor ruído permite que os espectadores apreciem outros sons da corrida, como o contato dos pneus com a pista e a interação entre os pilotos e suas equipes, oferecendo uma nova dimensão à experiência da corrida.</p>
+          </div>
+    
+          <img src="src/assets/foto-barulho.png" alt="ondas" />
+        </section>
+        <section className="section-2">
+            <div className="texto-2">
+              <p>Outro problema é o desconhecimento sobre a História da Fórmula E.
+              Embora a Fórmula E tenha feito avanços significativos desde seu lançamento em 2014, ainda há um grande número de pessoas que não conhecem sua história e seus objetivos. A falta de familiaridade com a evolução da Fórmula E e sua missão de promover a sustentabilidade e a inovação tecnológica pode limitar o engajamento e o interesse do público.</p>
+              <p>
+              A Fórmula E tem investido fortemente em campanhas de marketing e iniciativas educacionais para aumentar a conscientização sobre sua história e objetivos. Isso inclui a criação de documentários, séries de vídeos e artigos informativos que detalham a origem da Fórmula E, seus marcos importantes e suas contribuições para a sustentabilidade. Além disso, a Fórmula E tem fortalecido sua presença nas redes sociais e colaborado com influenciadores e mídias especializadas para alcançar um público mais amplo e diversificado.
+              </p>
+              
+            </div>
+            <Link to='/sobre' className="btn">Conhecer nossa história</Link>
+            
+
+            <img src="src/assets/redes-sociais.png" alt="redes" />
+        </section>
       
-      
+        
+        
 
 
-    </section>
+      </section>
+    </DesafiosStyle>
   )
 }
 
