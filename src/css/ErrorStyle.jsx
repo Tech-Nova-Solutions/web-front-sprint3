273ddf652586986fa07c1fda4af5d648f9963e37
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-09-26 20:34:27 -0300

    estilizando a pagina Error

---
+++ src/css/ErrorStyle.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ErrorStyle = styled.section `
+    width:100%;
+    height:100vh;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    font-size:2rem;
+    text-transform: uppercase;
+    background-image: linear-gradient(to top, var(--azul-claro), var(--azul-escuro));
+    color:var(--bg-color);
+
+`
--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -1,9 +1,11 @@
-
+import { ErrorStyle } from "../css/ErrorStyle"
 const Error = () => {
   return (
-    <>
+    <ErrorStyle>
+    <section>
         <h1>404- Página não encontrada</h1>
-    </>
+    </section>
+    </ErrorStyle>
   )
 }
 
